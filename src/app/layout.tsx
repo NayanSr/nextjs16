3417f4b82c1f681b8f11c9d889ae5d87a4f6e99d
Nayan Suter <nayankbps@gmail.com>
2025-12-04 09:02:17 +0600

    some small css issue in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-// import "./globals.css";
-// import "./custom_styles.css";
+import "./globals.css";
+import "./custom_styles.css";
 import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
